e0828b07a889fa7c65b1c9f79d1098d5b3410211
AugustasV <reg1nt1z@gmail.com>
2022-04-13 18:02:33 +0200

    updated_script
    Signed-off-by: AugustasV <reg1nt1z@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-## Zettelkasten repo
-
-Zet main page
+# zet
+Zettelkasten Repo. This is where I dump my knowledge as it happens, all my zettels ("slips" or notes) about almost anything and everything. The idea is rather simple really and very powerful. 
--- generate.py
+++ generate.py
@@ -1,43 +1,43 @@
 import os
-
+import re
 indexTextStart = """<!DOCTYPE html>
 <html>
-<head><title>Index of {folderPath}</title></head>
+<head><title>Index of Zettelkasten</title></head>
 <body>
-    <h2>Index of {folderPath}</h2>
+    <h2>Index of Zettelkasten</h2>
     <hr>
     <ul>
-		<li>
-			<a href='../'>../</a>
-		</li>
+        <li>
+            <a href='../'>../</a>
+        </li>
 """
 indexTextEnd = """
-	</ul>
+    </ul>
 </body>
 </html>
 """
 
 def index_folder(folderPath):
-	print("Indexing: " + folderPath +'/')
-	#Getting the content of the folder
-	files = os.listdir(folderPath)
-	#If Root folder, correcting folder name
-	root = folderPath
-	if folderPath == '.':
-		root = 'Root'
-	indexText = indexTextStart.format(folderPath=root)
-	for file in files:
-		#Avoiding index.html files
-		if file != 'index.html':
-			indexText += "\t\t<li>\n\t\t\t<a href='" + file + "'>" + file + "</a>\n\t\t</li>\n"
-		#Recursive call to continue indexing
-	# 	if os.path.isdir(folderPath+'/'+file):
-	# 		index_folder(folderPath + '/' + file)
-	# indexText += indexTextEnd
-	#Create or override previous index.html
-	index = open(folderPath+'/index.html', "w")
-	#Save indexed content to file
-	index.write(indexText)
+    print("Indexing: " + folderPath +'/')
+    #Getting the content of the folder
+    files = os.listdir(folderPath)
+    #If Root folder, correcting folder name
+    root = folderPath
+    if folderPath == '.':
+        root = 'Root'
+    indexText = indexTextStart.format(folderPath=root)
+
+    for file in files:
+        #Avoiding index.html files and indexing only numbers
+        if file != 'index.html' and re.sub(r'\D','',file):
+            f = open(file + '/README.md', 'r')
+            line = f.readline()
+            indexText += "\t\t<li>\n\t\t\t<a href='" + file + "'>" + re.sub('#','',line) + "</a>\n\t\t</li>\n"
+            f.close()
+    #Create or override previous index.html
+    index = open(folderPath+'/index.html', "w")
+    #Save indexed content to file
+    index.write(indexText)
 
 #Indexing root directory (Script position)
 index_folder('.')
